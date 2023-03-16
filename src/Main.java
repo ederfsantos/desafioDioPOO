260f64c045770ed6fa788d33e8c0072ec1d03060
EDER FERREIRA SANTOS <ederfsantos@live.com>
2023-03-16 15:39:54 -0300

    finalizando projeto

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import desafio.dominio.Conteudo;
-import desafio.dominio.Curso;
-import desafio.dominio.Mentoria;
+import desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,9 +22,44 @@ public class Main {
         m.setTitulo("aprendendo java");
         m.setData(LocalDate.now());
 
-        System.out.println(c);
+       /* System.out.println(c);
         System.out.println(c2);
-        System.out.println(m);
+        System.out.println(m);*/
+
+        Bootcamp bootcamp  = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(c);
+        bootcamp.getConteudos().add(c2);
+        bootcamp.getConteudos().add(m);
+
+        Dev eder = new Dev();
+        eder.setNome("Eder");
+        eder.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + eder.getConteudosInscritos());
+        eder.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + eder.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos " + eder.getConteudosConcluidos());
+        System.out.println("XP: " + eder.calcularXp());
+        System.out.println("------------------");
+        Dev joao = new Dev();
+        joao.setNome("João");
+        joao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + joao.getConteudosInscritos());
+       joao.progredir();
+       joao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + joao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos " + joao.getConteudosConcluidos());
+        System.out.println("XP: " + joao.calcularXp());
+
+
+
+
+
+
+
 
 
     }
